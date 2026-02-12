7ee93b4df0ab75b9a9bf3aa3e02d5d3119aca083
Peehu Mishra <peehumishra1308@gmail.com>
2026-02-12 19:33:51 +0530

    Updates

--- frontend/lib/services/api_service.dart
+++ frontend/lib/services/api_service.dart
@@ -2,15 +2,35 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  static Future<List<dynamic>> getOrders() async {
-    final response = await http.get(
-      Uri.parse('http://127.0.0.1:8000/orders'),
-    );
+  static const String baseUrl = "http://127.0.0.1:8000"; 
+  // Android emulator
+  // Use: http://192.168.X.X:8000 if using real phone
 
-    if (response.statusCode == 200) {
-      return jsonDecode(response.body);
-    } else {
-      throw Exception("Failed to load orders");
-    }
+  static Future<List> getOrders() async {
+    final res = await http.get(Uri.parse("$baseUrl/orders"));
+    return jsonDecode(res.body);
+  }
+
+  static Future<Map<String, dynamic>> getPrice(String symbol) async {
+    final res = await http.get(Uri.parse("$baseUrl/price/$symbol"));
+    return jsonDecode(res.body);
+  }
+
+  static Future<void> buyYesbank() async {
+    await http.post(
+      Uri.parse("$baseUrl/trade"),
+      headers: {"Content-Type": "application/json"},
+      body: jsonEncode({
+        "symbol": "YESBANK",
+        "exchange": "NSE",
+        "side": "BUY",
+        "quantity": 1,
+        "order_type": "LIMIT",
+        "validity": "DAY",
+        "streaming_symbol": "11536_NSE",
+        "limit_price": 25,
+        "product_code": "CNC"
+      }),
+    );
   }
 }
